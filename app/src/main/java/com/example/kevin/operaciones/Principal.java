909b639051fc84eed09314a8fde88b20c7052e64
Kevin Ariza <kevind199505@gmail.com>
2017-08-15 21:25:41 -0500

    Aplicacion operaciones solo suma terminada

--- app/src/main/java/com/example/kevin/operaciones/Principal.java
+++ app/src/main/java/com/example/kevin/operaciones/Principal.java
@@ -1,13 +1,59 @@
 package com.example.kevin.operaciones;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+    private TextView res;
+    private EditText n1, n2;
+    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        res = (TextView)findViewById(R.id.lblResultado);
+        n1 = (EditText)findViewById(R.id.txtNumeroUno);
+        n2 = (EditText)findViewById(R.id.txtNumeroDos);
+        resources = this.getResources();
+    }
+
+    public boolean validar(){
+        if (n1.getText().toString().isEmpty()){
+            n1.setError(resources.getString(R.string.mensaje_error_uno));
+            return false;
+        }
+        if (n2.getText().toString().isEmpty()){
+            n2.setError(resources.getString(R.string.mensaje_error_dos));
+            return false;
+        }
+
+        return true;
+    }
+
+    public void calular(View v){
+        int num1, num2, resultado;
+
+        if (validar()) {
+
+            num1 = Integer.parseInt(n1.getText().toString());
+            num2 = Integer.parseInt(n2.getText().toString());
+            resultado = num1 + num2;
+
+            res.setText("" + resultado);
+        }
+
+    }
+
+    public void borrar(View v){
+        res.setText("");
+        n1.setText("");
+        n2.setText("");
+        n1.requestFocus();
     }
 }
